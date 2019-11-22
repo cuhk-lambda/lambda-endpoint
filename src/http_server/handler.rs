ea4022d915f97358f754ac3e324dbb9e0e32b000
schrodingerzhu <i@zhuyi.fan>
2019-11-22 18:20:06 +0800

    dedup code

--- src/http_server/handler.rs
+++ src/http_server/handler.rs
@@ -16,7 +16,7 @@ use super::requests::*;
 fn verify_request(state: &State) -> Result<bool, String> {
     let headers = HeaderMap::borrow_from(state);
     headers.get("Authorization")
-        .ok_or("no header".to_string())
+        .ok_or("no authorization header".to_string())
         .and_then(|x| x
             .to_str()
             .map_err(|e| e.to_string()))
@@ -27,6 +27,44 @@ fn verify_request(state: &State) -> Result<bool, String> {
                 x[0] == global_config().endpoint_uuid && verify(y.as_str())
             } else { false }
         })
+}
+
+fn with_verification(state: State, todo: Box<dyn Fn(State) -> (State, Response<Body>)>) -> (State, Response<Body>) {
+    let verification = verify_request(&state);
+    match verification {
+        Ok(true) => todo(state),
+        Ok(false) => {
+            let json = serde_json::to_string(&ErrorReply { error: "unauthorized".to_string() }).unwrap();
+            let body = Body::from(json);
+            let res = create_response(&state, StatusCode::UNAUTHORIZED, mime::APPLICATION_JSON, body);
+            (state, res)
+        }
+        Err(e) => {
+            let json = ErrorReply { error: format!("{}", e) };
+            let body = Body::from(serde_json::to_string(&json).unwrap());
+            let res = create_response(&state, StatusCode::OK, mime::APPLICATION_JSON, body);
+            (state, res)
+        }
+    }
+}
+
+fn with_verification_res<E>(state: State, todo: Box<dyn Fn(State) -> Result<(State, Response<Body>), E>>) -> Result<(State, Response<Body>), E> {
+    let verification = verify_request(&state);
+    match verification {
+        Ok(true) => todo(state),
+        Ok(false) => {
+            let json = serde_json::to_string(&ErrorReply { error: "unauthorized".to_string() }).unwrap();
+            let body = Body::from(json);
+            let res = create_response(&state, StatusCode::UNAUTHORIZED, mime::APPLICATION_JSON, body);
+            Ok((state, res))
+        }
+        Err(e) => {
+            let json = ErrorReply { error: format!("{}", e) };
+            let body = Body::from(serde_json::to_string(&json).unwrap());
+            let res = create_response(&state, StatusCode::OK, mime::APPLICATION_JSON, body);
+            Ok((state, res))
+        }
+    }
 }
 
 pub fn heartbeat(state: State) -> (State, Response<Body>) {
@@ -65,77 +103,48 @@ pub fn endpoint_state(state: State) -> (State, Response<Body>) {
 pub fn trace_list(state: State) -> (State, Response<Body>) {
     use crate::db::schema::trace::traces::dsl::*;
     use crate::db::model::trace::*;
-    match verify_request(&state) {
-        Ok(true) => {
-            let conn = crate::db::connection::get_conn();
-            let result = traces
-                .load::<Trace>(&*conn).expect("failed to load trace");
-            let json = serde_json::to_string(&result).unwrap();
-            let body = Body::from(json);
-            let res = create_response(&state, StatusCode::OK, mime::APPLICATION_JSON, body);
-            (state, res)
-        }
-        Ok(false) => {
-            let json = serde_json::to_string(&ErrorReply { error: "unauthorized".to_string() }).unwrap();
-            let body = Body::from(json);
-            let res = create_response(&state, StatusCode::UNAUTHORIZED, mime::APPLICATION_JSON, body);
-            (state, res)
-        }
-        Err(e) => {
-            let json = ErrorReply { error: format!("{}", e) };
-            let body = Body::from(serde_json::to_string(&json).unwrap());
-            let res = create_response(&state, StatusCode::OK, mime::APPLICATION_JSON, body);
-            (state, res)
-        }
-    }
+    with_verification(state, box |state| {
+        let conn = crate::db::connection::get_conn();
+        let result = traces
+            .load::<Trace>(&*conn).expect("failed to load trace");
+        let json = serde_json::to_string(&result).unwrap();
+        let body = Body::from(json);
+        let res = create_response(&state, StatusCode::OK, mime::APPLICATION_JSON, body);
+        (state, res)
+    })
 }
 
 pub fn start_trace(mut state: State) -> Box<HandlerFuture> {
     use crate::db::schema::trace::traces::dsl::*;
     use crate::db::model::trace::*;
-    let verify = verify_request(&state);
     let body = Body::take_from(&mut state);
     let f = body.concat2().then(move |real| match real {
         Ok(x) => {
-            match verify {
-                Ok(true) => {
-                    let json =
-                        simd_json::serde::from_slice::<StartTrace>(x.to_vec().as_mut_slice());
-                    let reply = match json {
-                        Ok(e) => {
-                            let conn = crate::db::connection::get_conn();
-                            let result = traces.filter(id.eq(e.trace_id))
-                                .limit(1)
-                                .load::<Trace>(&*conn).expect("failed to load trace");
+            with_verification_res(state, box move |state| {
+                let json =
+                    simd_json::serde::from_slice::<StartTrace>(x.to_vec().as_mut_slice());
+                let reply = match json {
+                    Ok(e) => {
+                        let conn = crate::db::connection::get_conn();
+                        let result = traces.filter(id.eq(e.trace_id))
+                            .limit(1)
+                            .load::<Trace>(&*conn).expect("failed to load trace");
 
-                            if let Some(trace) = result.first() {
-                                let script = trace.run(e.lasting as _, e.trace_type.as_str());
-                                serde_json::to_string(&StartTraceReply { file_path: script })
-                            } else {
-                                serde_json::to_string(&ErrorReply { error: "no such trace".to_string() })
-                            }
+                        if let Some(trace) = result.first() {
+                            let script = trace.run(e.lasting as _, e.trace_type.as_str());
+                            serde_json::to_string(&StartTraceReply { file_path: script })
+                        } else {
+                            serde_json::to_string(&ErrorReply { error: "no such trace".to_string() })
                         }
-                        Err(k) => {
-                            serde_json::to_string(&ErrorReply { error: format!("error: {}", k) })
-                        }
-                    };
-                    let body = Body::from(reply.unwrap());
-                    let res = create_response(&state, StatusCode::OK, mime::APPLICATION_JSON, body);
-                    Ok((state, res))
-                }
-                Ok(false) => {
-                    let json = serde_json::to_string(&ErrorReply { error: "unauthorized".to_string() }).unwrap();
-                    let body = Body::from(json);
-                    let res = create_response(&state, StatusCode::UNAUTHORIZED, mime::APPLICATION_JSON, body);
-                    Ok((state, res))
-                }
-                Err(e) => {
-                    let json = serde_json::to_string(&ErrorReply { error: e }).unwrap();
-                    let body = Body::from(json);
-                    let res = create_response(&state, StatusCode::UNAUTHORIZED, mime::APPLICATION_JSON, body);
-                    Ok((state, res))
-                }
-            }
+                    }
+                    Err(k) => {
+                        serde_json::to_string(&ErrorReply { error: format!("error: {}", k) })
+                    }
+                };
+                let body = Body::from(reply.unwrap());
+                let res = create_response(&state, StatusCode::OK, mime::APPLICATION_JSON, body);
+                Ok((state, res))
+            })
         }
         Err(e) => {
             let json = ErrorReply { error: format!("{}", e) };
